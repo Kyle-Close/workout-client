7f3dd3b845abcfef3fa39f856da497d19412030c
Kyle-Close <k.james.close@gmail.com>
2026-01-15 08:54:40 -0800

    small update to base url and now logging data from server

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,19 @@ import { useQuery } from '@tanstack/react-query';
 import { BASE_URL, PROGRAM_ID, USER_ID } from './globals';
 
 function App() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ['repoData'],
-    queryFn: () =>
-      fetch(`${BASE_URL}/get-current-week-data`, {
-        body: JSON.stringify({ user_id: USER_ID, workout_program_id: PROGRAM_ID }),
-      }).then((res) => res.json()),
-  });
+	const { isPending, error, data } = useQuery({
+		queryKey: ['repoData'],
+		queryFn: () =>
+			fetch(`${BASE_URL}/get-current-week-data?user_id=${USER_ID}&workout_program_id=${PROGRAM_ID}`, {
+			}).then((res) => res.json()),
+	});
 
-  if (isPending) return 'Loading...';
-  if (error) return 'An error has occurred: ' + error.message;
+	if (isPending) return 'Loading...';
+	if (error) return 'An error has occurred: ' + error.message;
 
-  console.log(data);
+	console.log(data);
 
-  return <></>;
+	return <></>;
 }
 
 export default App;
--- src/globals.tsx
+++ src/globals.tsx
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://127.0.0.1:8000';
 export const USER_ID = 1;
 export const PROGRAM_ID = 1;

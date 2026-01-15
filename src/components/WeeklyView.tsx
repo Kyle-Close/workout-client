57caad97e427768f914587a2dbfc18c5060722ce
Kyle-Close <k.james.close@gmail.com>
2026-01-15 14:01:48 -0800

    added MUI and tailwind

--- src/components/WeeklyView.tsx
+++ src/components/WeeklyView.tsx
@@ -1,4 +1,6 @@
+import Typography from "@mui/material/Typography";
 import type { ExerciseForDayView } from "../schemas/currentWeekSchema";
+import Box from "@mui/material/Typography";
 
 interface WeeklyViewProps {
   currentDay: number;
@@ -9,9 +11,11 @@ export function WeeklyView({ currentDay, weekData }: WeeklyViewProps) {
   const weekNumber = weekData[0].program_week;
 
   return (
-    <div>
-      <h3>Week {weekNumber}</h3>
-      <h6>Day {currentDay}</h6>
-    </div>
+    <Box className="flex flex-row justify-between items-end">
+      <Typography className="text-amber-700" variant="h4">
+        Week {weekNumber}
+      </Typography>
+      <Typography variant="h6">Day {currentDay}</Typography>
+    </Box>
   );
 }

95542f423225aa90d2a1cd636794ae501648b680
Kyle-Close <k.james.close@gmail.com>
2026-01-18 13:41:03 -0800

    fix the base url for prod

--- src/globals.tsx
+++ src/globals.tsx
@@ -1,4 +1,4 @@
-//export const BASE_URL = 'https://workout-green-dust-7392.fly.dev';
-export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "https://workout-green-dust-7392.fly.dev";
+//export const BASE_URL = "http://127.0.0.1:8000";
 export const USER_ID = 1;
 export const PROGRAM_ID = 1;
